7f18aeddfff7d97eb9a4f1b3a6be6bb2793c9fca
Aristo Dakayao <da.aristo.y@gmail.com>
2024-11-20 14:34:19 +0100

    Auto-committed on 1.1.2

--- laravel-theme-generator.bat
+++ laravel-theme-generator.bat
@@ -35,11 +35,11 @@ if "%ACTION%"=="c" (
 
     rem Add the tag to the commit
     echo Adding tag %TAG_NAME%...
-    %GIT_PATH% push tag %TAG_NAME%
+    %GIT_PATH% tag %TAG_NAME%
 
     rem Push the changes to the remote branch
     rem echo Pushing changes to %BRANCH%...
-    rem %GIT_PATH% push %BRANCH%
+    %GIT_PATH% push %BRANCH%
 
     rem Push the tag to the remote repository
     rem echo Pushing tag %TAG_NAME%...
